ec2989c99024e4ef4d9b41070d54f46f6435b05a
App Academy Student <student@appacademy.io>
2015-07-13 13:47:00 -0700

    controller index

---
+++ RoutesDemo/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def index
+    render text: "I'm in the index action!"
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
