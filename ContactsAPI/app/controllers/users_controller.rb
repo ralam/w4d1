e02bdee3b494e245271f1153ed53eeb129df7b64
App Academy Student <student@appacademy.io>
2015-07-13 15:36:30 -0700

    write UsersController#create and #destroy

---
+++ ContactsAPI/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    render json: @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+end
--- ContactsAPI/app/models/user.rb
+++ ContactsAPI/app/models/user.rb
@@ -5,14 +5,16 @@ class User < ActiveRecord::Base
     :contacts,
     class_name: 'Contact',
     primary_key: :id,
-    foreign_key: :user_id
+    foreign_key: :user_id,
+    dependent: :destroy
   )
 
   has_many(
     :contact_shares,
     class_name: 'ContactShare',
     foreign_key: :user_id,
-    primary_key: :id
+    primary_key: :id,
+    dependent: :destroy
   )
 
   has_many :shared_contacts, through: :contact_shares, source: :contact
