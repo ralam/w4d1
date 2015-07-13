a5acfb73f6b2ea503b90ce2b0400ed570053dba3
App Academy Student <student@appacademy.io>
2015-07-13 15:40:04 -0700

    write UsersController#index, #show, #update

--- ContactsAPI/app/controllers/users_controller.rb
+++ ContactsAPI/app/controllers/users_controller.rb
@@ -15,6 +15,25 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username)
